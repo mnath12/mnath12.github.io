ee934e46fc264f1d3cfb953cdcd9d8ee07be9ae2
mnath12 <130778305+mnath12@users.noreply.github.com>
2023-12-26 01:16:36 -0500

    Tweaked basename

--- src/main.tsx
+++ src/main.tsx
@@ -41,7 +41,7 @@ const router = createHashRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter basename= {import.meta.env.PUBLIC_URL}>
+    <HashRouter basename= "/">
       <ChakraProvider>
       <App/>
 
