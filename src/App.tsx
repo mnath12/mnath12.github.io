aa233d25f8aed9aa393779cdb15fb62bf85af0a9
mnath12 <130778305+mnath12@users.noreply.github.com>
2023-12-26 00:17:50 -0500

    createBrowser Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import './App.css'
 import NavBar from './components/NavBar'
-import {Route, Routes} from 'react-router-dom'
-import Home from './pages/Home'
-import Sudoku from './pages/Sudoku'
-import Research from './pages/Research.tsx'
-import Reversi from './pages/Reversi'
+import {Outlet} from 'react-router-dom'
+
 
 function App() {
 
@@ -12,19 +9,8 @@ function App() {
     <>
    
     <NavBar></NavBar>
-    <div className='container'>
-      <Routes>
-        <Route path = '/' element = {<Home/>}/>
-        <Route path = '/projects' element = {<Sudoku/>}/>
-        <Route path = '/projects/sudoku' element = {<Sudoku/>}/>
-        <Route path = '/projects/reversi' element = {<Reversi/>}/>
-        <Route path = '/research' element = {<Research/>}/>
-
-      </Routes>
-    </div>
-
     
-    
+    <Outlet/>
      
      
     
--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,47 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
-import { HashRouter } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.tsx'
+import Sudoku from './pages/Sudoku.tsx'
+import Reversi from './pages/Reversi.tsx'
+import Research from './pages/Research.tsx'
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+              path: "/",
+              element: <Home/>
+            }, 
+            {
+              path: "/projects",
+              element: <Sudoku/>,
+              children: [{
+                  path: "/projects/sudoku",
+                  element: <Sudoku/>
+              },
+              {
+                  path: "/projects/reversi",
+                  element: <Reversi/>
+              }]
+            }, 
+            {
+              path: "/research",
+              element: <Research/>
+            }
+        ]
+
+    }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter  basename={import.meta.env.PUBLIC_URL}>
+    
       <ChakraProvider>
-        <App />
+      <RouterProvider router={router}/>
       </ChakraProvider>
-    </HashRouter>
-
   </React.StrictMode>,
 )
